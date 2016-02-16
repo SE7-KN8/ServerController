639485eba037637c803981ce53d4226cdf897f5e
SE7-KN8 <sebknack@gmail.com>
2016-02-16 20:36:34 +0100

    Alpha 0.1.6.7

--- Server Controller/src/sebe3012/servercontroller/gui/Frame.java
+++ Server Controller/src/sebe3012/servercontroller/gui/Frame.java
@@ -44,7 +44,7 @@ public class Frame extends Application {
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(this.getClass().getResource("style.css").toExternalForm());
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Minecraft Servercontroller by Sebe3012          Alpha 0.1.6.6");
+		primaryStage.setTitle("Minecraft Servercontroller by Sebe3012          Alpha 0.1.6.7");
 		primaryStage.setMaximized(true);
 		primaryStage.getIcons().add(new Image(this.getClass().getResource("icon.png").toExternalForm()));
 		primaryStage.setOnCloseRequest(event -> {
