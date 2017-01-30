a1a29e9f0055d3715169fa7106254b0da8c9a2d4
SE7-KN8 <sebknack@gmail.com>
2017-01-30 22:16:30 +0100

    Alpha 0.3.10.9

--- src/main/java/sebe3012/servercontroller/ServerController.java
+++ src/main/java/sebe3012/servercontroller/ServerController.java
@@ -37,7 +37,7 @@ public class ServerController {
 
 	public static boolean DEBUG = false;
 
-	public static final String VERSION = "0.3.9.9_alpha";
+	public static final String VERSION = "0.3.10.9_alpha";
 
 
 	private static final Logger log = LogManager.getLogger();
