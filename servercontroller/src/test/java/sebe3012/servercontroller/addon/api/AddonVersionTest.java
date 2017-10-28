c542246c7c06df44b930e32da146102764f456b6
SE7-KN8 <sebknack@gmail.com>
2017-10-28 18:18:00 +0200

    Added test support

---
+++ servercontroller/src/test/java/sebe3012/servercontroller/addon/api/AddonVersionTest.java
@@ -0,0 +1,14 @@
+package sebe3012.servercontroller.addon.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddonVersionTest {
+
+	@Test
+	public void compareTo() throws Exception {
+		assertEquals(0, 0);
+	}
+
+}
