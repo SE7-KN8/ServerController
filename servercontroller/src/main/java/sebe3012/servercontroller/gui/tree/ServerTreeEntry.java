4eaf5c78aae8341914afbf6381d5db3900f2a658
SE7-KN8 <sebknack@gmail.com>
2017-11-05 01:23:11 +0100

    Added context menu to TreeEntry
    Former-commit-id: 48e2215628f595f794dfeaae94cfafcb6f8dcedf

--- servercontroller/src/main/java/sebe3012/servercontroller/gui/tree/ServerTreeEntry.java
+++ servercontroller/src/main/java/sebe3012/servercontroller/gui/tree/ServerTreeEntry.java
@@ -1,11 +1,14 @@
 package sebe3012.servercontroller.gui.tree;
 
 import sebe3012.servercontroller.server.BasicServer;
+import sebe3012.servercontroller.util.I18N;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.shape.Circle;
 
 /**
@@ -16,7 +19,7 @@ public class ServerTreeEntry implements TreeEntry<BasicServer> {
 
 	private BasicServer item;
 
-	public ServerTreeEntry(BasicServer item){
+	public ServerTreeEntry(BasicServer item) {
 		this.item = item;
 	}
 
@@ -30,6 +33,27 @@ public class ServerTreeEntry implements TreeEntry<BasicServer> {
 	@Override
 	public Node getGraphic() {
 		return new Circle(8, item.getState().getColor());
+	}
+
+	@Nullable
+	@Override
+	public ContextMenu getContextMenu() {
+
+		MenuItem start = new MenuItem(I18N.translate("tooltip_start_server"));
+		start.setOnAction(e -> item.getServerHandler().onStartClicked());
+
+		MenuItem restart = new MenuItem(I18N.translate("tooltip_restart_server"));
+		restart.setOnAction(e -> item.getServerHandler().onRestartClicked());
+
+		MenuItem stop = new MenuItem(I18N.translate("tooltip_stop_server"));
+		stop.setOnAction(e -> item.getServerHandler().onStopClicked());
+
+		ContextMenu menu = new ContextMenu();
+		menu.getItems().add(start);
+		menu.getItems().add(restart);
+		menu.getItems().add(stop);
+
+		return menu;
 	}
 
 	@NotNull
