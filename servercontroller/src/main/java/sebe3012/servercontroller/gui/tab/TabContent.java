dacb4f87d4b3add902a7a266fc3bcb8043222ea8
SE7-KN8 <sebknack@gmail.com>
2017-11-07 19:58:07 +0100

    Better way to implement the last commit.
    Former-commit-id: 5f5ce9a8e6d29250b36fb05a73d981844b7a1396

--- servercontroller/src/main/java/sebe3012/servercontroller/gui/tab/TabContent.java
+++ servercontroller/src/main/java/sebe3012/servercontroller/gui/tab/TabContent.java
@@ -2,11 +2,7 @@ package sebe3012.servercontroller.gui.tab;
 
 import sebe3012.servercontroller.util.I18N;
 
-import org.fxmisc.flowless.VirtualizedScrollPane;
-import org.fxmisc.richtext.CodeArea;
-
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
@@ -22,19 +18,6 @@ public class TabContent {
 			this.handler = new TabContentHandler(this);
 			loader.setController(handler);
 			root = loader.load();
-
-			CodeArea area = new CodeArea();
-			area.setId("cOutput");
-			area.setPrefHeight(400);
-			area.setPrefWidth(800);
-			VirtualizedScrollPane<CodeArea> pane = new VirtualizedScrollPane<>(area, ScrollPane.ScrollBarPolicy.AS_NEEDED, ScrollPane.ScrollBarPolicy.ALWAYS);
-			GridPane.setRowIndex(pane, 1);
-
-			root.getChildren().add(pane);
-			handler.initCodeArea(area);
-			//<CodeArea fx:id="cOutput" editable="false" prefHeight="414.0" prefWidth="823.0" GridPane.rowIndex="1" />
-
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
