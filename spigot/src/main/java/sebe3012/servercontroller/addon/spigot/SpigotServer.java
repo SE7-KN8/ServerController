01f864e6b2a6c0c9ca71e22bbfcb32656a4b0c01
SE7-KN8 <sebknack@gmail.com>
2017-11-30 23:03:22 +0100

    Spigot Addon 0.0.1.2 Works with new api
    Former-commit-id: afc0fd8d41a69c24de8339cee4b0672bd6546737

--- spigot/src/main/java/sebe3012/servercontroller/addon/spigot/SpigotServer.java
+++ spigot/src/main/java/sebe3012/servercontroller/addon/spigot/SpigotServer.java
@@ -1,5 +1,6 @@
 package sebe3012.servercontroller.addon.spigot;
 
+import sebe3012.servercontroller.addon.api.Addon;
 import sebe3012.servercontroller.addon.craftbukkit.CraftbukkitServer;
 
 import javafx.beans.property.StringProperty;
@@ -9,8 +10,8 @@ import java.util.Map;
 public class SpigotServer extends CraftbukkitServer {
 	private StringProperty spigotConfig;
 
-	public SpigotServer(Map<String, StringProperty> properties){
-		super(properties);
+	public SpigotServer(Map<String, StringProperty> properties, Addon addon){
+		super(properties, addon);
 
 		spigotConfig = properties.get("spigot");
 	}
