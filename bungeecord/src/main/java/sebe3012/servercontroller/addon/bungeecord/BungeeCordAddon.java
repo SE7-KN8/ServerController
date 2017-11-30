c8961fc3ba492b39da670c338d859ef12f294e35
SE7-KN8 <sebknack@gmail.com>
2017-11-30 23:05:02 +0100

    BungeeCord Addon 0.0.1.2

--- bungeecord/src/main/java/sebe3012/servercontroller/addon/bungeecord/BungeeCordAddon.java
+++ bungeecord/src/main/java/sebe3012/servercontroller/addon/bungeecord/BungeeCordAddon.java
@@ -5,7 +5,6 @@ import sebe3012.servercontroller.addon.api.AddonUtil;
 import sebe3012.servercontroller.addon.api.DialogRow;
 import sebe3012.servercontroller.addon.api.ServerCreator;
 import sebe3012.servercontroller.addon.api.StringPredicates;
-import sebe3012.servercontroller.eventbus.IEventHandler;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -16,7 +15,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class BungeeCordAddon extends Addon implements IEventHandler, ServerCreator {
+public class BungeeCordAddon extends Addon implements ServerCreator {
 
 	@Override
 	public void load() {
