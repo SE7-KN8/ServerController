f94ad2a65929a5ccad53d58fd49ea5d3757df752
SE7-KN8 <sebknack@gmail.com>
2018-01-28 17:38:37 +0100

    Added "server/start|stop|restart" and "server/state" endpoint.
    Former-commit-id: 77b89e6b476d13fdb3ac4d7c3f7c6da5e6a94414

---
+++ servercontroller-rest-api/src/main/java/se7kn8/servercontroller/api/rest/ServerControllerServerState.java
@@ -0,0 +1,13 @@
+package se7kn8.servercontroller.api.rest;
+
+public class ServerControllerServerState {
+	private String state;
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+}
