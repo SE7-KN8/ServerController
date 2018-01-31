546f13aded29bec648be14e54f79e47d6d2af9fc
SE7-KN8 <sebknack@gmail.com>
2018-01-31 11:40:07 +0100

    Added json serialization to Permission.
    Former-commit-id: a7e0fb262358491cfc46040d31f3af825e70150c

--- servercontroller-core/src/test/java/se7kn8/servercontroller/rest/authentication/permission/PermissionTest.java
+++ servercontroller-core/src/test/java/se7kn8/servercontroller/rest/authentication/permission/PermissionTest.java
@@ -6,6 +6,8 @@ import se7kn8.servercontroller.rest.authentication.permission.node.NamedPermissi
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -16,11 +18,14 @@ public class PermissionTest {
 	private Permission pem2;
 	private Permission pem3;
 
+	private Gson gson;
+
 	@Before
 	public void setUp() {
 		pem1 = new Permission();
 		pem2 = new Permission(new NamedPermissionNode("servercontroller"), new NamedPermissionNode("test"), new NamedPermissionNode("permission"), new GlobNode());
 		pem3 = new Permission(new NamedPermissionNode("servercontroller"), new NamedPermissionNode("permission"), new NamedPermissionNode("set"));
+		gson = new Gson();
 	}
 
 	@Test
@@ -49,5 +54,19 @@ public class PermissionTest {
 
 		assertTrue(pem3.isPermissionValid("servercontroller.permission.set"));
 		assertFalse(pem3.isPermissionValid("servercontroller.permission.get"));
+	}
+
+	@Test
+	public void toJson() {
+		assertEquals("{\"nodes\":[]}", Permission.toJson(pem1).toString());
+		assertEquals("{\"nodes\":[{\"type\":\"NAMED\",\"path\":\"servercontroller\"},{\"type\":\"NAMED\",\"path\":\"test\"},{\"type\":\"NAMED\",\"path\":\"permission\"},{\"type\":\"GLOB\",\"path\":\"*\"}]}", Permission.toJson(pem2).toString());
+		assertEquals("{\"nodes\":[{\"type\":\"NAMED\",\"path\":\"servercontroller\"},{\"type\":\"NAMED\",\"path\":\"permission\"},{\"type\":\"NAMED\",\"path\":\"set\"}]}", Permission.toJson(pem3).toString());
+	}
+
+	@Test
+	public void fromJson() {
+		assertEquals(pem1.getPermission(), Permission.fromJson(Permission.toJson(pem1)).getPermission());
+		assertEquals(pem2.getPermission(), Permission.fromJson(Permission.toJson(pem2)).getPermission());
+		assertEquals(pem3.getPermission(), Permission.fromJson(Permission.toJson(pem3)).getPermission());
 	}
 }
