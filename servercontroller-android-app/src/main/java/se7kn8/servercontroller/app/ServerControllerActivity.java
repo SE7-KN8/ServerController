f73ba51da6047469b5b305d383cc244b634a57f8
SE7-KN8 <sebknack@gmail.com>
2018-02-12 22:06:10 +0100

    Added basic style and toolbar.
    Former-commit-id: 93b0b8142cb9f4d1acf298259f2cf9fcb4d42d01

---
+++ servercontroller-android-app/src/main/java/se7kn8/servercontroller/app/ServerControllerActivity.java
@@ -0,0 +1,16 @@
+package se7kn8.servercontroller.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ServerControllerActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+}
